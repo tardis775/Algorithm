68c21e8474cc22f55dfdbc3e2a6fdcca2f64302b
tardis775 <tardis775@naver.com>
2020-01-16 15:57:37 +0900

    Algorithm008_4th_Completed

--- Algorithm008.py
+++ Algorithm008.py
@@ -1,4 +1,4 @@
-#문제
+#region 문제
 # JOI는 물리, 화학, 생물, 지구과학, 역사, 지리 총 6 과목의 시험을 봤다. 각 시험의 만점은 100점이다.
 # JOI는 물리, 화학, 생물, 지구과학 4과목 중에서 3 과목을 선택하고 역사, 지리 2 과목 중에서 한 과목을 선택한다.
 # 시험 점수의 합이 가장 높게 나오도록 과목을 선택할 때, JOI가 선택한 과목의 시험 점수의 합을 구하시오.
@@ -15,6 +15,8 @@
 
 #출력
 # JOI가 선택한 과목의 총 점수를 1행에 출력하시오.
+
+#endregion
 
 Physics = input("물리점수를 입력하시오.     : ")
 Chemistry = input("화학점수를 입력하시오.     : ")
@@ -50,6 +52,7 @@ def Calculation(Score):
 Calculation(Science)
 Calculation(Society)
 
+#region 초기 코딩
 '''
 count1 = 0
 count2 = 0
@@ -81,6 +84,7 @@ for i in Society:
     count1 += 1
     count2 = count1
 '''
+#endregion
 
 sum = 0
 
